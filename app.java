b47ebe53ba3649af68838a8b5d7d06e808b548c1
Mrinmoy-27 <105783469+Mrinmoy-27@users.noreply.github.com>
2022-08-19 14:03:41 +0530

    Add files via upload

---
+++ app.java
@@ -0,0 +1,296 @@
+package mrinmoy.java;
+
+public class app {
+	public static void main(String args[]) 
+	
+	/*{System.out.println("Mrinmoy Sarkar");
+	System.out.println("Mrinmoy Sarkar");
+	byte ms=124;
+	int sm= 112;
+	int n=(ms+sm);
+	System.out.println(n);
+	long k=100000L;
+	short b= 2222;
+	System.out.print(k);}*/
+	/*{int a=22;
+	double b=a;// widning casting
+	System.out.println(b);
+	
+		double c=8.9;
+	int d=(int)c;//Narrowing casting
+	System.out.print(d); 
+	}*/
+	/*{int a=12, b=22;
+	int sum=(a+b);
+	long sub=(a-b);
+	System.out.println(sub);
+	System.out.println(sum);}*/
+	/*{int a=70,b=90;
+	System.out.println(a>b);
+	System.out.println(a<b);
+	System.out.println(a>=b);//
+	System.out.println(a<=b);	
+	System.out.println(a==b);
+	System.out.println(a!=b);
+	}*/
+	/*{int a=70, b=80, c=90;
+	System.out.println(a>b && b<c);// false && false= false
+	System.out.println(a>b || b<c);//
+	System.out.println(!(a>b || b<c));
+	}*/
+	/*{int i=0;
+	i= i++ - --i + ++i- i--;
+	System.out.print(i);
+	}*/
+	/*{
+		int i=11;
+		i=i++ + ++i;
+		System.out.println(i);
+		
+		int a=11,b=22,c;
+		c=a+b+a++ + b++ + ++a + ++b;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("c="+c);
+		
+		int v=11;
+		System.out.println("the value of v=" +v);
+		
+	}*/
+	{
+		  {
+		     /*oolean b = true;
+		         
+		        b++;
+		         
+		        System.out.println(b);*/
+		        
+		       /*nt i=1, j=2, k=3;
+		         
+		        int m = i-- - j-- - k--;
+		         
+		        System.out.println("i="+i);
+		        System.out.println("j="+j);
+		        System.out.println("k="+k);
+		        System.out.println("m="+m);*/
+		        
+		       /*t a=1, b=2;
+		        System.out.println(--b - ++a + ++b - --a);*/
+		        
+		       /*nt i=19, j=29, k;
+		        k = i-- - i++ + --j - ++j + --i - j-- + ++i - j++;    
+		        System.out.println("i="+i);
+		        System.out.println("j="+j);
+		        System.out.println("k="+k);*/
+			  
+			  /*nt i = 11;
+		       int j = --(i++);
+		       System.out.println(j);*/
+		        
+		      /*nt m = 0, n = 0;
+		       int p = --m * --n * n-- * m--;
+		       System.out.println(p);*/
+			 
+			 /*nt a=1;
+		      a = a++ + ++a * --a - a--;
+		      System.out.println(a);*/
+			  
+			/*nt m=1010, n=1010;
+		      System.out.println(m++ / ++n * n-- / --m); */
+			  
+			/* int x = 001, y=010, z=100;
+		         
+		        int i = --x + y++ - z-- - --z + ++y - --x + y-- - --x;
+		         
+		        System.out.println("x="+x);
+		        System.out.println("y="+y);
+		        System.out.println("z="+z);
+		        System.out.println("i="+i);*/
+			 //area of the  
+			   /*nt r=5;
+		        double pi = 3.14, area;
+		        area = pi * r * r;      			// For area of circle
+		        System.out.println("Area of circle:"+area); 
+		        
+		        float f=112.5f;
+		        float c=( (f-32)*5/9);				// For conversation F to C
+		        System.out.println(c+ " Celcious");
+			  
+		        double i=22,m;
+		        m=(i/39.37);						// For Inch to Meter
+		        System.out.println(m+" meter");*/
+			  
+			 /* int age=17;
+			   String result=age>=18?"Eligible to vote":"Not eligible to vote";
+			    System.out.println(result);*/
+			  
+			/*  int num=7;
+			  String result= num<0?"Negetive number":"Positive Number";
+			  System.out.println(result);
+			  
+			  int x=20;
+			  int y=12;
+			  if(x+y<30) {
+				  System.out.println("sum is less than 30");
+				  
+			  }else {
+				  System.out.println("sum is greater than 30");}
+			  
+			  int f=32,k=12;
+			  if(f<k) {
+				  System.out.println("f is smaller than k");
+			  }
+			  else {
+				  System.out.println("K is smaller than F");
+			  }
+			  */
+			  
+			 /* int a=7,b=90,c=23;
+			  if(a>b && a>c) {
+				  System.out.println(a+" is greatest");
+			  }else if(b>a && b>c) {
+				  System.out.println(b+" is greatest");
+			  }else {
+				  System.out.println(c+" is greatest");
+			  }*/
+			  
+			 /* if(condition) { //outer-if
+					if(condition) { //inner-if
+						//statements
+						
+					}else if(condition) {
+						//statements
+					}else if(condition) {
+						//statements
+					}
+					
+					else {
+						//statements
+					}
+				}else if(condition) {
+					//statements
+				}
+
+				else {
+					if(condition) {
+						//statements
+					}else if(condition) {
+						//statements
+					}*/
+					//statements
+			  /*int a=78,b=100,c=88;
+			  if(a>b) {
+				  if(a>c) {
+					  System.out.println(a+"is greatest");
+				  }else {
+					  System.out.println(c+"is greatest");
+				  }
+			  }else if(b>a) {
+				  if(b>c) {
+					  System.out.println(b+"is greatest");
+				  }else {
+					  System.out.println(c+"is greatest");
+				  }
+			  }else {
+				  System.out.println(c+"is greatest");
+			  }*/
+			  
+			/*  int a=24, b=33;
+			  if (a%2==0) {
+				  if(a<0) {
+					  System.out.println(a+" is an even & negetiive number");
+				  			}
+				  else {
+					  System.out.print(a+" is even  & positive number");
+				  }}
+				  else {
+					  System.out.print(a+" is not even nor negetive");
+				  }*/
+			  
+		 //***The formula to find the roots of the quadratic equation is known as the quadratic formula.***
+			  int a=1,b=5,c=1;
+			  double d=((Math.sqrt(b*b))-(4*a*c));
+			  System.out.println(d);
+			  if(d<0) {
+				  System.out.println("both roots are distinct and imaginary or complex.");
+			  }
+			  else if(d==0) {
+				  System.out.println("both roots are real and the same.");
+			  }
+			  else if (d>0){
+				  System.out.println("both roots are real and different.");
+			  }
+			  
+		  //***Write a Java Program to check whether a number is divisible by 5 as well as 3***
+			  int e=25;
+			  if(e%3==0) {
+				  if(e%5==0) {
+					  System.out.println("it can be divide by both 3 and 5");
+				  }
+				  System.out.println("It only can be devide by 3");
+				} 
+			  else if(e%5==0) {
+				  System.out.println("It can only devide by 5");
+			  }
+			  else {
+				  System.out.println("It cannot");
+			  }
+			  
+		  //***Write a Java Program to take marks of a student and then print his/her grade depending on the following conditions:
+			  /*90 to 100 then grade is A+, 
+			  80 to 90  then grade is A
+			  70 to 80 then grade is B+
+			  60 to 70 then grade is B
+			  50 to 60 then grade is C
+			  40 to 50 then grade is D
+			  30 to 40 then grade is E
+			  less than 30 then grade is F*/
+			  
+			  int g=55;
+			  if(g>=90 && g<=100) {
+				  System.out.println("The grade is: A+");
+			  }
+			  else if(g>=80 && g<90) {
+				  System.out.println("The grade is: A");
+			  }
+			  else if(g>=70 && g<80) {
+				  System.out.println("The grade is: B+");
+			  }
+			  else if(g>=60 && g<70) {
+				  System.out.println("The grade is: B");
+			  }
+			  else if(g>=50 && g<60) {
+				  System.out.println("The grade is: C");
+			  }
+			  else if(g>=40 && g<50) {
+				  System.out.println("The grade is: D");
+			  }
+			  else if(g>=30 && g<40) {
+				  System.out.println("The grade is: E");
+			  }
+			  else if( g<30) {
+				  System.out.println("The grade is: F");
+			  }
+			  
+		//Write a Java Program to find whether a number is even or odd using conditional operator
+			  int h=9;
+			  if(h%2==0) {
+				  System.out.println("It is an EVEN number.");
+			  }
+			  else {
+				  System.out.println("It is an ODD number.");
+			  }
+			  
+	    //Write a Java program that takes four integers and prints equal if all four are equal, and not equal otherwise.
+			  
+			  int i=2,j=2,k=2,l=2;
+			  if(i==j && j==k &&  k==l) {
+				  System.out.println("All are equal");
+			  }
+			  else {
+				  System.out.println("All are not Equal");
+			  }
+		  }
+	}
+	
+}
